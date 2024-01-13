9cb69e2e64e3967f207c1123ef14ce2f3fd33ad2
Benature <wbenature@163.com>
2024-01-13 20:04:36 +0800

    update PyPDF

--- pdf_filter/filter2.py
+++ pdf_filter/filter2.py
@@ -6,17 +6,15 @@
 import os
 import numpy as np
 from pdf2image import convert_from_path
-from PyPDF2 import PdfFileWriter, PdfFileReader
-
+from PyPDF2 import PdfReader, PdfWriter
 
 # ========== Config ==================
-path = "path/to/lec/pdf"
+path = "D:\\new\\talk.pdf"
 ignore_files = [
     "pdf_that_u_don't_want_to_filtrate1.pdf",
     "pdf_that_u_don't_want_to_filtrate2.pdf"
 ]
 # ====================================
-
 
 THRESHOLD_PAGE = 0.1
 ANTITHESIS = 255
@@ -28,17 +26,17 @@ FLAG = "[s]"
 def splitPdf(file_path, pages):
     assert file_path[-4:] == '.pdf'
     root_path, file_name = get_dir_name(file_path)
-    output = PdfFileWriter() 
-    pdf_raw = PdfFileReader(open(file_path, "rb")) 
-    for i in pages: 
-        output.addPage(pdf_raw.getPage(i)) 
-    outputStream = open(root_path + FLAG + file_name, "wb") 
+    output = PdfWriter()
+    pdf_raw = PdfReader(open(file_path, "rb"))
+    for i in pages:
+        output.add_page(pdf_raw.pages[i])
+    outputStream = open(root_path + FLAG + file_name, "wb")
     output.write(outputStream)
 
 
 def get_dir_name(file_dir):
-    base_name = os.path.basename(file_dir) 
-    dir_name = os.path.dirname(file_dir) + "/" 
+    base_name = os.path.basename(file_dir)
+    dir_name = os.path.dirname(file_dir) + "/"
     return dir_name, base_name
 
 
@@ -55,7 +53,7 @@ def pdf_filter(file_path):
     for ind in range(len(arrs) - 1):
         diff_area = arrs[ind] - arrs[ind + 1]
         diff_rate = (diff_area != 0).sum() / SIZE
-        diff_x, diff_y = np.where(diff_area != 0)   
+        diff_x, diff_y = np.where(diff_area != 0)
         DIFF_strict = False  # 严格判断
         for x, y in zip(diff_x, diff_y):
             if np.abs(arrs[ind][x, y] - ANTITHESIS) > THRESHOLD_COLOR:
@@ -70,7 +68,7 @@ def pdf_filter(file_path):
     splitPdf(file_path, change_pages)
 
 
-def main(print_tips = True):
+def main(print_tips=True):
     if path[-4:] != ".pdf":
         # a path of folder
         root_path = path.replace("\\", "/").rstrip("/") + "/"  # 以防万一最后没加斜杠
@@ -93,14 +91,16 @@ def main(print_tips = True):
             fn = str(fn)
             print("begin filtrate:", fn)
             if print_tips:
-                print("if you don't want to filtrate this file, \nyou can `ctrl+c` to kill the mission \nand add the file name in the `ignore_file` list.\n")
+                print(
+                    "if you don't want to filtrate this file, \nyou can `ctrl+c` to kill the mission \nand add the file name in the `ignore_file` list.\n"
+                )
             pdf_filter(root_path + fn)
     else:
         # single one pdf file
         assert path[-4:] == '.pdf', "are you sure it is a pdf file?"
         print("begin filtrate:", path)
         pdf_filter(path)
-    print("\n", "="*30)
+    print("\n", "=" * 30)
     print("Finish Filtrating!")
     print("Thank you for your using and waiting")
 
